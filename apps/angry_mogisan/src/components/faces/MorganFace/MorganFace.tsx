db38be14432e5c110fe4b4aa7943d6047b2099ff
Debens <a.debens@gmail.com>
2022-10-01 13:22:15 +0100

    feat(angry_mogisan): adding morgan face pack

--- apps/angry_mogisan/src/components/faces/MorganFace/MorganFace.tsx
+++ apps/angry_mogisan/src/components/faces/MorganFace/MorganFace.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Image, ImageSourcePropType, StyleSheet } from 'react-native';
 
-import { Grid, Paragraph } from '@training/mobile-atoms/src';
+import { Grid } from '@training/mobile-atoms/src';
 
+import { FaceEmotion } from '../../GameProvider/game-context';
 import { FaceProps } from '../model';
+
+const FACE_SOURCES: Record<FaceEmotion, ImageSourcePropType> = {
+    [FaceEmotion.Neutral]: require('./images/neutral.png'),
+    [FaceEmotion.Calm]: require('./images/calm.png'),
+    [FaceEmotion.Angry]: require('./images/angry.png'),
+};
 
 const MorganFace: React.FunctionComponent<FaceProps> = props => {
     const { emotion } = props;
 
+    const size = emotion === FaceEmotion.Angry ? styles.large : styles.regular;
+
     return (
-        <Grid flex={1} margin="small" borderRadius="medium">
-            <Paragraph textAlgin="center">Morgan is {emotion}</Paragraph>
+        <Grid variant="center" flex={1} margin="small" borderRadius="medium">
+            <Image style={size} source={FACE_SOURCES[emotion]} />
         </Grid>
     );
 };
+
+const styles = StyleSheet.create({
+    regular: { width: '125%', height: '125%', overflow: 'visible' },
+    large: { width: '175%', height: '175%', overflow: 'visible' },
+});
 
 export default MorganFace;
